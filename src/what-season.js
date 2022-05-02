57483fcec0e194a28e7ccd1c0b13bfd053b161ea
Julia Antoshkina <julia.antoshkina@gmail.com>
2022-05-02 23:08:12 +0200

    part2

--- src/what-season.js
+++ src/what-season.js
@@ -11,16 +11,31 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
+
+function dateCheck(date) {
+  if (Object.prototype.toString.call(date) === "[object Date]") {
+    // it is a date
+    if (isNaN(date)) {
+      // d.getTime() or d.valueOf() will also work
+      return false;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
 function getSeason(date) {
   throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
 
   if (!date) {
     return "Unable to determine the time of year!";
-  } else if (date.length !== 6) {
+  } else if (!dateCheck(date)) {
     return "Invalid date!";
   }
-  let month = getMonth(date);
+
+  let month = date.getMonth();
   if (month === 0 || month === 1 || month > 10) {
     return "winter";
   } else if (month === 2 || month === 3 || month === 4) {
